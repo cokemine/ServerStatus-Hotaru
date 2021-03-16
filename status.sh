a8720466b8310887adfd3add3a0d8cc77cf63ac8
Subeting <68261543+Subeting@users.noreply.github.com>
2021-03-16 23:12:56 +0800

    换源
    更换了下载源，加速国内方向的下载

--- status.sh
+++ status.sh
@@ -80,7 +80,7 @@ check_region() {
 }
 Download_Server_Status_server() {
   cd "/tmp" || exit 1
-  wget -N --no-check-certificate "https://github.com/CokeMine/ServerStatus-Hotaru/archive/master.zip"
+  wget -N --no-check-certificate "https://ghproxy.com/https://github.com/CokeMine/ServerStatus-Hotaru/archive/master.zip"
   [[ ! -e "master.zip" ]] && echo -e "${Error} ServerStatus 服务端下载失败 !" && exit 1
   unzip master.zip
   rm -rf master.zip
@@ -115,7 +115,7 @@ Download_Server_Status_server() {
 }
 Download_Server_Status_client() {
   cd "/tmp" || exit 1
-  wget -N --no-check-certificate "https://raw.githubusercontent.com/CokeMine/ServerStatus-Hotaru/master/clients/status-client.py"
+  wget -N --no-check-certificate "https://cdn.jsdelivr.net/gh/CokeMine/ServerStatus-Hotaru/clients/status-client.py"
   [[ ! -e "status-client.py" ]] && echo -e "${Error} ServerStatus 客户端下载失败 !" && exit 1
   cd "${file_1}" || exit 1
   [[ ! -e "${file}" ]] && mkdir "${file}"
@@ -142,14 +142,14 @@ Download_Server_Status_client() {
 }
 Service_Server_Status_server() {
   if [[ ${release} == "centos" ]]; then
-    if ! wget --no-check-certificate "https://raw.githubusercontent.com/CokeMine/ServerStatus-Hotaru/master/service/server_status_server_centos" -O /etc/init.d/status-server; then
+    if ! wget --no-check-certificate "https://cdn.jsdelivr.net/gh/CokeMine/ServerStatus-Hotaru/service/server_status_server_centos" -O /etc/init.d/status-server; then
       echo -e "${Error} ServerStatus 服务端服务管理脚本下载失败 !" && exit 1
     fi
     chmod +x /etc/init.d/status-server
     chkconfig --add status-server
     chkconfig status-server on
   else
-    if ! wget --no-check-certificate "https://raw.githubusercontent.com/CokeMine/ServerStatus-Hotaru/master/service/server_status_server_debian" -O /etc/init.d/status-server; then
+    if ! wget --no-check-certificate "https://cdn.jsdelivr.net/gh/CokeMine/ServerStatus-Hotaru/service/server_status_server_debian" -O /etc/init.d/status-server; then
       echo -e "${Error} ServerStatus 服务端服务管理脚本下载失败 !" && exit 1
     fi
     chmod +x /etc/init.d/status-server
@@ -159,14 +159,14 @@ Service_Server_Status_server() {
 }
 Service_Server_Status_client() {
   if [[ ${release} == "centos" ]]; then
-    if ! wget --no-check-certificate "https://raw.githubusercontent.com/CokeMine/ServerStatus-Hotaru/master/service/server_status_client_centos" -O /etc/init.d/status-client; then
+    if ! wget --no-check-certificate "https://cdn.jsdelivr.net/gh/CokeMine/ServerStatus-Hotaru/service/server_status_client_centos" -O /etc/init.d/status-client; then
       echo -e "${Error} ServerStatus 客户端服务管理脚本下载失败 !" && exit 1
     fi
     chmod +x /etc/init.d/status-client
     chkconfig --add status-client
     chkconfig status-client on
   else
-    if ! wget --no-check-certificate "https://raw.githubusercontent.com/CokeMine/ServerStatus-Hotaru/master/service/server_status_client_debian" -O /etc/init.d/status-client; then
+    if ! wget --no-check-certificate "https://cdn.jsdelivr.net/gh/CokeMine/ServerStatus-Hotaru/service/server_status_client_debian" -O /etc/init.d/status-client; then
       echo -e "${Error} ServerStatus 客户端服务管理脚本下载失败 !" && exit 1
     fi
     chmod +x /etc/init.d/status-client
@@ -782,9 +782,9 @@ Modify_config_client() {
 Install_jq() {
   if [[ ! -e ${jq_file} ]]; then
     if [[ ${bit} == "x86_64" ]]; then
-      wget --no-check-certificate "https://github.com/stedolan/jq/releases/download/jq-1.5/jq-linux64" -O ${jq_file}
+      wget --no-check-certificate "https://hub.fastgit.org/stedolan/jq/releases/download/jq-1.5/jq-linux64" -O ${jq_file}
     else
-      wget --no-check-certificate "https://github.com/stedolan/jq/releases/download/jq-1.5/jq-linux32" -O ${jq_file}
+      wget --no-check-certificate "https://hub.fastgit.org/stedolan/jq/releases/download/jq-1.5/jq-linux32" -O ${jq_file}
     fi
     [[ ! -e ${jq_file} ]] && echo -e "${Error} JQ解析器 下载失败，请检查 !" && exit 1
     chmod +x ${jq_file}
@@ -793,7 +793,7 @@ Install_jq() {
     echo -e "${Info} JQ解析器 已安装，继续..."
   fi
   if [[ ! -e ${region_json} ]]; then
-    wget --no-check-certificate "https://raw.githubusercontent.com/michaelwittig/node-i18n-iso-countries/master/langs/zh.json" -O ${region_json}
+    wget --no-check-certificate "https://cdn.jsdelivr.net/gh/michaelwittig/node-i18n-iso-countries/langs/zh.json" -O ${region_json}
     [[ ! -e ${region_json} ]] && echo -e "${Error} ISO 3166-1 json文件下载失败，请检查！" && exit 1
   fi
 }
@@ -806,7 +806,7 @@ Install_caddy() {
     Set_server "server"
     Set_server_http_port
     if [[ ! -e "/usr/local/caddy/caddy" ]]; then
-      wget -N --no-check-certificate https://raw.githubusercontent.com/CokeMine/ServerStatus-Hotaru/master/caddy/caddy_install.sh
+      wget -N --no-check-certificate https://cdn.jsdelivr.net/gh/CokeMine/ServerStatus-Hotaru/caddy/caddy_install.sh
       chmod +x caddy_install.sh
       bash caddy_install.sh install
       rm -rf caddy_install.sh
@@ -969,7 +969,7 @@ Uninstall_ServerStatus_server() {
     rm -rf "/etc/init.d/status-server"
     if [[ -e "/etc/init.d/caddy" ]]; then
       /etc/init.d/caddy stop
-      wget -N --no-check-certificate https://raw.githubusercontent.com/CokeMine/ServerStatus-Hotaru/master/caddy/caddy_install.sh
+      wget -N --no-check-certificate https://cdn.jsdelivr.net/gh/CokeMine/ServerStatus-Hotaru/caddy/caddy_install.sh
       chmod +x caddy_install.sh
       bash caddy_install.sh uninstall
       rm -rf caddy_install.sh
@@ -1091,7 +1091,7 @@ Set_iptables() {
   fi
 }
 Update_Shell() {
-  sh_new_ver=$(wget --no-check-certificate -qO- -t1 -T3 "https://raw.githubusercontent.com/CokeMine/ServerStatus-Hotaru/master/status.sh" | grep 'sh_ver="' | awk -F "=" '{print $NF}' | sed 's/\"//g' | head -1)
+  sh_new_ver=$(wget --no-check-certificate -qO- -t1 -T3 "https://cdn.jsdelivr.net/gh/CokeMine/ServerStatus-Hotaru/status.sh" | grep 'sh_ver="' | awk -F "=" '{print $NF}' | sed 's/\"//g' | head -1)
   [[ -z ${sh_new_ver} ]] && echo -e "${Error} 无法链接到 Github !" && exit 0
   if [[ -e "/etc/init.d/status-client" ]]; then
     rm -rf /etc/init.d/status-client
@@ -1101,7 +1101,7 @@ Update_Shell() {
     rm -rf /etc/init.d/status-server
     Service_Server_Status_server
   fi
-  wget -N --no-check-certificate "https://raw.githubusercontent.com/CokeMine/ServerStatus-Hotaru/master/status.sh" && chmod +x status.sh
+  wget -N --no-check-certificate "https://cdn.jsdelivr.net/gh/CokeMine/ServerStatus-Hotaru/status.sh" && chmod +x status.sh
   echo -e "脚本已更新为最新版本[ ${sh_new_ver} ] !(注意：因为更新方式为直接覆盖当前运行的脚本，所以可能下面会提示一些报错，无视即可)" && exit 0
 }
 menu_client() {

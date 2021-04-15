56b55874f0cb7214bfab73c858cd106ca140dff9
CokeMine <aptx4561@gmail.com>
2021-04-15 10:48:55 +0800

    refactor: update status-client.py

--- clients/status-client.py
+++ clients/status-client.py
@@ -2,56 +2,48 @@
 # Support Python Version 2.7 to 3.7
 # Update by: https://github.com/CokeMine/ServerStatus-Hotaru
 
+import socket
+import time
+import re
+import os
+import json
+import subprocess
+from collections import deque
+
 SERVER = "127.0.0.1"
 PORT = 35601
 USER = "USER"
 PASSWORD = "USER_PASSWORD"
 INTERVAL = 1  # 更新间隔，单位：秒
 
-import socket
-import time
-import string
-import math
-import re
-import os
-import json
-import subprocess
-import collections
-import platform
-
 
 def get_uptime():
-    f = open('/proc/uptime', 'r')
-    uptime = f.readline()
-    f.close()
-    uptime = uptime.split('.', 2)
-    time = int(uptime[0])
-    return int(time)
+    with open('/proc/uptime', 'r') as f:
+        uptime = f.readline().split('.')
+    return int(uptime[0])
 
 
 def get_memory():
-    re_parser = re.compile(r'^(?P<key>\S*):\s*(?P<value>\d*)\s*kB')
+    re_parser = re.compile(r'(\S*):\s*(\d*)\s*kB')
     result = dict()
     for line in open('/proc/meminfo'):
         match = re_parser.match(line)
-        if not match:
-            continue
-        key, value = match.groups(['key', 'value'])
-        result[key] = int(value)
+        if match:
+            result[match.group(1)] = int(match.group(2))
 
-    MemTotal = float(result['MemTotal'])
-    MemFree = float(result['MemFree'])
-    Cached = float(result['Cached'])
-    MemUsed = MemTotal - (Cached + MemFree)
-    SwapTotal = float(result['SwapTotal'])
-    SwapFree = float(result['SwapFree'])
-    return int(MemTotal), int(MemUsed), int(SwapTotal), int(SwapFree)
+    mem_total = float(result['MemTotal'])
+    mem_free = float(result['MemFree'])
+    cached = float(result['Cached'])
+    mem_used = mem_total - (cached + mem_free)
+    swap_total = float(result['SwapTotal'])
+    swap_free = float(result['SwapFree'])
+    return int(mem_total), int(mem_used), int(swap_total), int(swap_free)
 
 
 def get_hdd():
     p = subprocess.check_output(
         ['df', '-Tlm', '--total', '-t', 'ext4', '-t', 'ext3', '-t', 'ext2', '-t', 'reiserfs', '-t', 'jfs', '-t', 'ntfs',
-         '-t', 'fat32', '-t', 'btrfs', '-t', 'fuseblk', '-t', 'zfs', '-t', 'simfs', '-t', 'xfs']).decode("Utf-8")
+         '-t', 'fat32', '-t', 'btrfs', '-t', 'fuseblk', '-t', 'zfs', '-t', 'simfs', '-t', 'xfs']).decode('utf-8')
     total = p.splitlines()[-1]
     used = total.split()[3]
     size = total.split()[2]
@@ -59,117 +51,87 @@ def get_hdd():
 
 
 def get_load():
-    try:
-        return round(os.getloadavg()[0] * 2) / 2
-    except:
-        return -1.0
+    return round(os.getloadavg()[0], 1)
 
 
-def get_time():
-    stat_file = open("/proc/stat", "r")
-    time_list = stat_file.readline().split(' ')[2:6]
-    stat_file.close()
+def get_cpu_time():
+    with open('/proc/stat', 'r') as stat_file:
+        time_list = stat_file.readline().split()[1:]
     time_list = list(map(int, time_list))
-    return time_list
-
-
-def delta_time():
-    x = get_time()
-    time.sleep(INTERVAL)
-    y = get_time()
-    for i in range(len(x)):
-        y[i] -= x[i]
-    return y
+    return sum(time_list), time_list[3]
 
 
 def get_cpu():
-    t = delta_time()
-    st = sum(t)
-    if st == 0:
-        st = 1
-    result = 100 - (float(t[len(t) - 1]) * 100.00 / st)
-    return round(result, 1)
+    old_total, old_idle = get_cpu_time()
+    time.sleep(INTERVAL)
+    total, idle = get_cpu_time()
+    return round(100 - float(idle - old_idle) / (total - old_total) * 100.00, 1)
 
 
-class Traffic:
+class Network:
     def __init__(self):
-        self.rx = collections.deque(maxlen=10)
-        self.tx = collections.deque(maxlen=10)
+        self.rx = deque(maxlen=10)
+        self.tx = deque(maxlen=10)
 
-    def get(self):
-        f = open('/proc/net/dev', 'r')
-        net_dev = f.readlines()
-        f.close()
-        avgrx = 0
-        avgtx = 0
+    def get_traffic(self):
+        net_in = 0
+        net_out = 0
+        re_parser = re.compile(r'([^\s]+):[\s]*(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+('
+                               r'\d+)\s+(\d+)\s+(\d+)')
+        with open('/proc/net/dev') as f:
+            for line in f.readlines():
+                net_info = re_parser.findall(line)
+                if net_info:
+                    if check_interface(net_info[0][0]):
+                        net_in += int(net_info[0][1])
+                        net_out += int(net_info[0][9])
+        self.rx.append(net_in)
+        self.tx.append(net_out)
+        return net_in, net_out
 
-        for dev in net_dev[2:]:
-            dev = dev.split(':')
-            if dev[0].strip() == "lo" or dev[0].find("tun") > -1:
-                continue
-            dev = dev[1].split()
-            avgrx += int(dev[0])
-            avgtx += int(dev[8])
-
-        self.rx.append(avgrx)
-        self.tx.append(avgtx)
-        avgrx = 0
-        avgtx = 0
-
-        l = len(self.rx)
-        for x in range(l - 1):
-            avgrx += self.rx[x + 1] - self.rx[x]
-            avgtx += self.tx[x + 1] - self.tx[x]
-
-        avgrx = int(avgrx / l / INTERVAL)
-        avgtx = int(avgtx / l / INTERVAL)
-
-        return avgrx, avgtx
+    def get_speed(self):
+        avg_rx = 0
+        avg_tx = 0
+        queue_len = len(self.rx)
+        for x in range(queue_len - 1):
+            avg_rx += self.rx[x + 1] - self.rx[x]
+            avg_tx += self.tx[x + 1] - self.tx[x]
+        avg_rx = int(avg_rx / queue_len / INTERVAL)
+        avg_tx = int(avg_tx / queue_len / INTERVAL)
+        return avg_rx, avg_tx
 
 
-def liuliang():
-    NET_IN = 0
-    NET_OUT = 0
-    with open('/proc/net/dev') as f:
-        for line in f.readlines():
-            netinfo = re.findall(
-                '([^\s]+):[\s]{0,}(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)',
-                line)
-            if netinfo:
-                if netinfo[0][0] == 'lo' or 'tun' in netinfo[0][0] or netinfo[0][1] == '0' or netinfo[0][9] == '0':
-                    continue
-                else:
-                    NET_IN += int(netinfo[0][1])
-                    NET_OUT += int(netinfo[0][9])
-    return NET_IN, NET_OUT
+def check_interface(net_name):
+    net_name = net_name.strip()
+    invalid_name = ['lo', 'tun', 'kube', 'docker', 'vmbr', 'br-', 'vnet', 'veth']
+    return not any(name in net_name for name in invalid_name)
 
 
 def get_network(ip_version):
-    if (ip_version == 4):
-        HOST = "ipv4.google.com"
-    elif (ip_version == 6):
-        HOST = "ipv6.google.com"
+    if ip_version == 4:
+        host = 'ipv4.google.com'
+    elif ip_version == 6:
+        host = 'ipv6.google.com'
     else:
         return False
     try:
-        socket.create_connection((HOST, 80), 2).close()
+        socket.create_connection((host, 80), 2).close()
         return True
-    except:
-        pass
-    return False
+    except Exception:
+        return False
 
 
 if __name__ == '__main__':
     socket.setdefaulttimeout(30)
     while True:
         try:
-            print("Connecting...")
+            print('Connecting...')
             s = socket.create_connection((SERVER, PORT))
             data = s.recv(1024).decode()
-            if data.find("Authentication required") > -1:
-                s.send((USER + ':' + PASSWORD + '\n').encode("utf-8"))
+            if data.find('Authentication required') > -1:
+                s.send((USER + ':' + PASSWORD + '\n').encode('utf-8'))
                 data = s.recv(1024).decode()
-                if data.find("Authentication successful") < 0:
+                if data.find('Authentication successful') < 0:
                     print(data)
                     raise socket.error
             else:
@@ -177,25 +139,26 @@ if __name__ == '__main__':
                 raise socket.error
 
             print(data)
-            data = s.recv(1024).decode()
-            print(data)
+            if data.find('You are connecting via') < 0:
+                data = s.recv(1024).decode()
+                print(data)
 
             timer = 0
             check_ip = 0
-            if data.find("IPv4") > -1:
+            if data.find('IPv4') > -1:
                 check_ip = 6
-            elif data.find("IPv6") > -1:
+            elif data.find('IPv6') > -1:
                 check_ip = 4
             else:
                 print(data)
                 raise socket.error
 
-            traffic = Traffic()
-            traffic.get()
+            traffic = Network()
+            traffic.get_traffic()
             while True:
                 CPU = get_cpu()
-                NetRx, NetTx = traffic.get()
-                NET_IN, NET_OUT = liuliang()
+                NET_IN, NET_OUT = traffic.get_traffic()
+                NetRx, NetTx = traffic.get_speed()
                 Uptime = get_uptime()
                 Load = get_load()
                 MemoryTotal, MemoryUsed, SwapTotal, SwapFree = get_memory()
@@ -204,7 +167,7 @@ if __name__ == '__main__':
                 array = {}
                 if not timer:
                     array['online' + str(check_ip)] = get_network(check_ip)
-                    timer = 10
+                    timer = 120
                 else:
                     timer -= 1 * INTERVAL
 
@@ -221,18 +184,17 @@ if __name__ == '__main__':
                 array['network_tx'] = NetTx
                 array['network_in'] = NET_IN
                 array['network_out'] = NET_OUT
-
-                s.send(("update " + json.dumps(array) + "\n").encode("utf-8"))
+                s.send(("update " + json.dumps(array) + '\n').encode('utf-8'))
         except KeyboardInterrupt:
             raise
         except socket.error:
-            print("Disconnected...")
+            print('Disconnected...')
             # keep on trying after a disconnect
             if 's' in locals().keys():
                 del s
             time.sleep(3)
         except Exception as e:
-            print("Caught Exception:", e)
+            print('Caught Exception:', e)
             if 's' in locals().keys():
                 del s
             time.sleep(3)
